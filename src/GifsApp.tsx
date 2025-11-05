2a21711060548dab180a66423237493bc7d6a39d
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-11-04 21:02:31 -0500

    add axios and httt rquest to external api

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,22 +1,45 @@
+import { useState } from "react"
 import { GifsList } from "./gifs/components/GifsList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions"
+import type { Gif } from "./gifs/interfaces/giph.interface"
 
 
 export const GifsApp = () => {
+  const [previosTerms, setPreviosTerms] = useState(['dragon ball z']);
+  const [data, setData] = useState<Gif[]>([]);
+
+
+  const handleTermClicked = (term: string) => {
+    console.log({term})
+  }
+
+  const handleSearch = async (query: string) => {
+    const term = query.trim().toLowerCase();
+    if(!term) return;
+    if (previosTerms.includes(term)) return;
+    setPreviosTerms((prev) => {
+      const updated = [term, ...prev];
+      return updated.slice(0,8);
+    });
+
+    const gifs = await getGifsByQuery(query);
+    setData(gifs);
+  }
+
   return (
     <>
       <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el gif perfecto"/>
       {/* Search */}
-      <SearchBar placeholder="Busca lo que quieras"/>
+      <SearchBar placeholder="Busca lo que quieras" onQuery={handleSearch}/>
       
 
       {/* Busquedas previas */}
-      <PreviousSearches searches={['Goku', 'Dragon Ball Z']} />
+      <PreviousSearches searches={previosTerms} onLabelClicked={handleTermClicked}/>
       {/* Gifs */}
-      <GifsList gifs={mockGifs} />
+      <GifsList gifs={data} />
     </>
   )
 }
